e22dc1cfc02d8152d5c0f4406bbdada1bf3d9b2f
earmendariz-upna <enrique.armendariz@unavarra.es>
2020-10-04 14:08:06 +0000

    Hemos incluido una nueva clase Mensaje para sacar por pantalla otro mensaje

---
+++ src/pruebaholamundo/Mensaje.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaholamundo;
+
+/**
+ *
+ * @author profesor
+ */
+public class Mensaje {
+    String mensaje = "Hola Mundo";
+}
--- src/pruebaholamundo/PruebaHolaMundo.java
+++ src/pruebaholamundo/PruebaHolaMundo.java
@@ -15,7 +15,10 @@ public class PruebaHolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");    
+        System.out.println("Hola Mundo"); 
+        Mensaje m = new Mensaje();
+        System.out.println("*** Despues de crear la instancia de Mensaje");
+        System.out.println("Mensaje: " + m.mensajes);
     }
     
 }
